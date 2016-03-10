655076bbd224d470eb39f1f75332c078096006f9
lewissmallwood <lewis@smallwood.it>
2016-03-10 15:15:33 +0000

    I have no idea if this will work.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # packt-claimer
-A utility for claiming your free packt book automagically.
+This is a web utility for claiming your Packt book automagically.
+
+Everday Packt Publishing release a free book at: https://www.packtpub.com/packt/offers/free-learning
+
+This NodeJS server, based apon the code from [Fabio](https://github.com/draconar/)'s repository, [grab_packt](https://github.com/draconar/grab_packt/), allows for a web request on port 80 to be sent to this Node.JS application in order to claim your book.
+
+As this runs on a web server, it means that (<b>if you're happy passing through your details in plaintext</b>), it can be run in conjunction with IFTTT.
+
+^ <i>Obviously, not so secure.</i>
+
+## Installing
+
+Install this script in the cloned directory using the following command:
+
+    npm install
+
+After that run the script with the following command:
+
+    watch -n 5000 --differences node server.js
+    
+Then send a web request to: `http://localhost/?email=EMAIL@HERE.COM&password=PASSWORD`
+
+^ <i>This link then can be called from other services such as [IFTTT](https://ifttt.com/channels/maker/actions/1600703425-make-a-web-request) to claim your book when certian actions occur.</i>
---
+++ server.js
@@ -0,0 +1,93 @@
+require('dotenv').load({
+    path: __dirname + '/.env'
+});
+
+var request = require('request');
+var cheerio = require('cheerio');
+var http = require('http');
+
+const PORT = 80;
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+    console.log("The web server started and is listening on: http://localhost:%s", PORT);
+});
+
+function handleRequest(req, response){
+      var loginDetails = {
+          email: "",
+          password: "",
+          op: "Login",
+          form_id: "packt_user_login_form",
+          form_build_id: ""
+      };
+
+      var url = 'https://www.packtpub.com/packt/offers/free-learning';
+      var loginError = 'Sorry, you entered an invalid email address and password combination.';
+      var getBookUrl;
+      var bookTitle;
+
+      // We need cookies for that, therefore let's turn JAR on
+      request = request.defaults({
+          jar: true
+      });
+
+      loginDetails.email = "";
+      loginDetails.password = "";
+
+      console.log('----------- Packt Grab Started -----------');
+      request(url, function(err, res, body) {
+          if (err) {
+              console.error('Request failed');
+              console.log('----------- Packt Grab Done --------------');
+              return;
+          }
+
+          var $ = cheerio.load(body);
+          getBookUrl = $("a.twelve-days-claim").attr("href");
+          bookTitle = $(".dotd-title").text().trim();
+          var newFormId = $("input[type='hidden'][id^=form][value^=form]").val();
+
+          if (newFormId) {
+              loginDetails.form_build_id = newFormId;
+          }
+
+          request.post({
+              uri: url,
+              headers: {
+                  'content-type': 'application/x-www-form-urlencoded'
+              },
+              body: require('querystring').stringify(loginDetails)
+          }, function(err, res, body) {
+              if (err) {
+                  console.error('Login failed');
+                  console.log('----------- Packt Grab Done --------------');
+                  return;
+              };
+              var $ = cheerio.load(body);
+              var loginFailed = $("div.error:contains('"+loginError+"')");
+              if (loginFailed.length) {
+                  console.error('Login failed, please check your email address and password');
+                  console.log('Login failed, please check your email address and password');
+                  console.log('----------- Packt Grab Done --------------');
+                  return;
+              }
+
+              request('https://www.packtpub.com' + getBookUrl, function(err, res, body) {
+                  if (err) {
+                      console.error('Request Error');
+                      console.log('----------- Packt Grab Done --------------');
+                      return;
+                  }
+
+                  var $ = cheerio.load(body);
+
+                  console.log('Book Title: ' + bookTitle);
+                  console.log('Claim URL: https://www.packtpub.com' + getBookUrl);
+                  console.log('----------- Packt Grab Done --------------');
+              });
+          });
+      });
+
+    response.end('It Works!! Path Hit: ' + req.url);
+}
